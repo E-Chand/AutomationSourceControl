924b446b20542a384a175fc822d850f9c89a97c6
E-Chand <73599949+E-Chand@users.noreply.github.com>
2022-07-14 17:19:28 -0400

    Update runbookwrapper.ps1

--- runbookwrapper.ps1
+++ runbookwrapper.ps1
@@ -10,6 +10,8 @@ $AzureContext = (Connect-AzAccount -Identity -AccountId '99f47f9f-7603-4b9a-818c
 # set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
+Write-Output "Line added to check source code integration"
+
 Write-Output ""
 Write-Output "Executing runbook to copy SQL db"
 Write-Output ""
